141a2632c2389776319f839b4766009e126a7b83
Mauricio Rodrigues <grtorres17@gmail.com>
2021-08-27 18:34:50 -0300

    feat: add global styles

---
+++ src/styles/GlobalStyles.ts
@@ -0,0 +1,18 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    outline: 0;
+    padding: 0;
+  }
+
+  html, body, #root {
+    min-height: 100%;
+  }
+
+  body {
+    -webkit-font-smoothing: antialiased !important;
+  }
+`;
