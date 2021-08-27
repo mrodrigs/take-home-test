400d5a34b12e69717ad15fc9b4aa06e641dd18fa
Mauricio Rodrigues <grtorres17@gmail.com>
2021-08-27 18:22:29 -0300

    ref: fix linter errors

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
-export default MyApp
+const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
+  return <Component {...pageProps} />;
+};
+export default MyApp;
