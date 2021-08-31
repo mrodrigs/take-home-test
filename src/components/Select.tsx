02b6afd7dc5b37dba8f1c3b64ded4ac8187bebdd
Mauricio Rodrigues <grtorres17@gmail.com>
2021-08-31 18:01:37 -0300

    feat: create multiple select component

---
+++ src/components/Select.tsx
@@ -0,0 +1,85 @@
+import { useEffect, useState } from 'react';
+
+import Checkbox from 'components/Checkbox';
+import {
+  CheckboxWrapper,
+  Container,
+  DropdownIcon,
+  Select,
+  SelectWrapper,
+  Text,
+} from 'styles/components/Select';
+import ContactCategory from 'types/interfaces/ContactCategories';
+
+interface SelectInputProps {
+  contactCategories: ContactCategory[];
+}
+
+const SelectInput: React.FC<SelectInputProps> = ({ contactCategories }) => {
+  const [currentCategories, setCurrentCategories] =
+    useState<ContactCategory[]>(contactCategories);
+  const [isAllSelected, setIsAllSelected] = useState<boolean>(true);
+  const [isExpanded, setIsExpanded] = useState<boolean>(false);
+
+  const handleCheckCategory = (index: number) => {
+    const newCurrentCategories = [...currentCategories];
+
+    newCurrentCategories[index].checked = !newCurrentCategories[index].checked;
+
+    setCurrentCategories(newCurrentCategories);
+  };
+
+  const toggleSelectAllCategories = () => {
+    const currentStatus = isAllSelected;
+
+    const newCurrentCategories = currentCategories.map(category => {
+      return { ...category, checked: !currentStatus };
+    });
+
+    setCurrentCategories(newCurrentCategories);
+
+    setIsAllSelected(!currentStatus);
+  };
+
+  const toggleShowCheckboxes = () => {
+    setIsExpanded(currentStatus => !currentStatus);
+  };
+
+  useEffect(() => {
+    const hasUncheckedCategory = currentCategories?.find(
+      category => category.checked === false,
+    );
+
+    setIsAllSelected(!hasUncheckedCategory);
+  }, [currentCategories]);
+
+  return (
+    <Container isExpanded={isExpanded}>
+      <Select isExpanded={isExpanded} onClick={toggleShowCheckboxes}>
+        <SelectWrapper>
+          <Checkbox
+            appearance="outline"
+            checked={isAllSelected}
+            onChange={toggleSelectAllCategories}
+          />
+          <Text>All contacts</Text>
+        </SelectWrapper>
+        <DropdownIcon isExpanded={isExpanded} />
+      </Select>
+      <CheckboxWrapper isExpanded={isExpanded}>
+        {currentCategories.map((category, index) => (
+          <Checkbox
+            appearance="default"
+            checked={category.checked}
+            id={category.id}
+            key={category.id}
+            label={category.label}
+            onChange={() => handleCheckCategory(index)}
+          />
+        ))}
+      </CheckboxWrapper>
+    </Container>
+  );
+};
+
+export default SelectInput;
---
+++ src/styles/components/Select.ts
@@ -0,0 +1,82 @@
+import styled from 'styled-components';
+
+interface CheckboxWrapperProps {
+  isExpanded: boolean;
+}
+
+interface ContainerProps {
+  isExpanded: boolean;
+}
+
+interface DropdownIconProps {
+  isExpanded: boolean;
+}
+
+interface SelectProps {
+  isExpanded: boolean;
+}
+
+export const CheckboxWrapper = styled.div<CheckboxWrapperProps>`
+  background: #f5f8fa;
+  border: 1px solid #cbd6e2;
+  border-top: none;
+  height: ${({ isExpanded }) => (isExpanded ? '111px' : '0')};
+  overflow-y: scroll;
+  padding-left: 17px;
+  transition: height 0.4s;
+
+  ::-webkit-scrollbar {
+    width: 13px;
+  }
+
+  ::-webkit-scrollbar-thumb {
+    background-clip: padding-box;
+    background-color: #cbd6e2;
+    border: 5px solid rgba(0, 0, 0, 0);
+    border-radius: 50px;
+  }
+
+  ::-webkit-scrollbar-track {
+    background: transparent;
+  }
+`;
+
+export const Container = styled.div<ContainerProps>`
+  padding-bottom: ${({ isExpanded }) => (isExpanded ? '33px' : '66px')};
+  transition: padding-bottom 0.4s;
+  width: 200px;
+`;
+
+export const DropdownIcon = styled.img.attrs({
+  src: '/assets/icons/dropdownIcon.svg',
+})<DropdownIconProps>`
+  transform: ${({ isExpanded }) => (isExpanded ? `rotate(-180deg)` : '')};
+  transition: all 0.3s ease-out;
+`;
+
+export const Select = styled.div<SelectProps>`
+  align-items: center;
+  background: #f5f8fa;
+  border: 1px solid #cbd6e2;
+  border-bottom: none;
+  border-radius: ${({ isExpanded }) => (isExpanded ? '2px 2px 0 0' : '2px')};
+  box-sizing: border-box;
+  color: #33475b;
+  cursor: pointer;
+  display: flex;
+  font-size: 16px;
+  height: 44px;
+  justify-content: space-between;
+  padding: 0 20px 0 17px;
+  width: 100%;
+`;
+
+export const SelectWrapper = styled.div`
+  align-items: center;
+  display: flex;
+  flex-direction: row;
+`;
+
+export const Text = styled.div`
+  padding-left: 17px;
+`;
