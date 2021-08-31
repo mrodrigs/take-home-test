48687bb1f13f0d63f3d1b47b05190dade02103a8
Mauricio Rodrigues <grtorres17@gmail.com>
2021-08-31 17:56:15 -0300

    feat: create common styled components

---
+++ src/styles/Common.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Title = styled.span`
+  color: #374a5e;
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: 25px;
+  margin-bottom: 14.5px;
+  text-align: center;
+`;
