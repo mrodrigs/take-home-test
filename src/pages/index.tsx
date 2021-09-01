840062403c573a0abe576408c182fa8d997e0839
Mauricio Rodrigues <grtorres17@gmail.com>
2021-09-01 01:31:26 -0300

    feat: update tab name

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,29 +5,35 @@ import mailchimpCategories from 'api/mailchimpCategories';
 import Background from 'components/Background';
 import ServiceCard from 'components/ServiceCard';
 import SyncButton from 'components/SyncButton';
+import Head from 'next/head';
 import { Container } from 'styles/pages/Home';
 
 const Home: NextPage = () => {
   return (
-    <Background>
-      <Container>
-        <ServiceCard
-          contactCategories={gmailCategories}
-          image="/assets/images/gmailIcon.svg"
-          imageAlt="Gmail icon"
-          text="These Gmail contacts will sync to MailChimp"
-          title="Gmail"
-        />
-        <SyncButton />
-        <ServiceCard
-          contactCategories={mailchimpCategories}
-          image="/assets/images/mailchimpIcon.svg"
-          imageAlt="Mailchimp icon"
-          text="These Mailchimp contacts will sync to Gmail"
-          title="Mailchimp"
-        />
-      </Container>
-    </Background>
+    <>
+      <Head>
+        <title>Take home test</title>
+      </Head>
+      <Background>
+        <Container>
+          <ServiceCard
+            contactCategories={gmailCategories}
+            image="/assets/images/gmailIcon.svg"
+            imageAlt="Gmail icon"
+            text="These Gmail contacts will sync to MailChimp"
+            title="Gmail"
+          />
+          <SyncButton />
+          <ServiceCard
+            contactCategories={mailchimpCategories}
+            image="/assets/images/mailchimpIcon.svg"
+            imageAlt="Mailchimp icon"
+            text="These Mailchimp contacts will sync to Gmail"
+            title="Mailchimp"
+          />
+        </Container>
+      </Background>
+    </>
   );
 };
 
