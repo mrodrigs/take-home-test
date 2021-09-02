e9ae2201b6eb8669b8372860e75ef0c3dbfb6b73
Mauricio Rodrigues <mauricio.rodrigues@gobots.com.br>
2021-09-02 02:10:31 -0300

    test: create service categories mock

---
+++ src/__mocks__/serviceCategories.ts
@@ -0,0 +1,21 @@
+import ContactCategory from 'types/interfaces/ContactCategories';
+
+const serviceCategories: ContactCategory[] = [
+  {
+    checked: true,
+    id: 'mock-1',
+    label: 'Mock Label 1',
+  },
+  {
+    checked: false,
+    id: 'mock-2',
+    label: 'Mock Label 2',
+  },
+  {
+    checked: true,
+    id: 'mock-3',
+    label: 'Mock Label 3',
+  },
+];
+
+export default serviceCategories;
